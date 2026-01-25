749ce4cd0a7830e0648f982c06fc314e31f823c0
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-01-25 12:31:15 -0500

    Added MatchPeriod enum

--- src/main/java/frc/robot/utils/MatchTimer.java
+++ src/main/java/frc/robot/utils/MatchTimer.java
@@ -1,6 +1,14 @@
 package frc.robot.utils;
 
 public final class MatchTimer {
+  public static enum MatchPeriod {
+    kAuto,
+    kTransition,
+    kShiftActive,
+    kShiftInactive,
+    kEndgame;
+  }
+
   private MatchTimer() {
     throw new UnsupportedOperationException("This is a utility class!");
   }
