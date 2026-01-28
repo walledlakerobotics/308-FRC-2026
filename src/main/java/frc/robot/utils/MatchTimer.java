ea31cc6c062be3676e791b7c6fcc67e07c1a7431
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-01-28 15:08:50 -0500

    Check if robot is disabled and return disabled match period in getMatchPeriod method

--- src/main/java/frc/robot/utils/MatchTimer.java
+++ src/main/java/frc/robot/utils/MatchTimer.java
@@ -64,6 +64,7 @@ public final class MatchTimer {
   }
 
   public MatchPeriod getMatchPeriod() {
+    if (DriverStation.isDisabled()) return MatchPeriod.Disabled;
     if (DriverStation.isAutonomousEnabled()) return MatchPeriod.Autonomous;
     if (DriverStation.getMatchType() == MatchType.None) return MatchPeriod.TeleopUnknown;
 
