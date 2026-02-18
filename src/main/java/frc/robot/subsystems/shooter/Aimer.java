d1dafe519f9f5c018730ec55be24e41b89c3912a
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-02-18 18:15:03 -0500

    Add basic motor setup

--- src/main/java/frc/robot/subsystems/shooter/Aimer.java
+++ src/main/java/frc/robot/subsystems/shooter/Aimer.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.shooter;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants.AimerConstants;
 
-public class Aimer extends SubsystemBase {}
+public class Aimer extends SubsystemBase {
+  private final SparkMax m_aimerMotor =
+      new SparkMax(AimerConstants.kAimerCanId, MotorType.kBrushless);
+
+  public Aimer() {
+    m_aimerMotor.configure(
+        Configs.Aimer.aimerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+}
