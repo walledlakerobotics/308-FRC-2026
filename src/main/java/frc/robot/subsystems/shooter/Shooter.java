3a64c78d7f7057ec78772d5b77b753d1fb260111
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-02-18 14:24:26 -0500

    Add inital leader and follower motor setup

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants.ShooterConstants;
 
-public class Shooter extends SubsystemBase {}
+public class Shooter extends SubsystemBase {
+  TalonFX m_shooterLeader = new TalonFX(ShooterConstants.kShooterLeaderCanId);
+  TalonFX m_shooterFollower = new TalonFX(ShooterConstants.kShooterFollowerCanId);
+
+  public Shooter() {
+    m_shooterLeader.getConfigurator().apply(Configs.Shooter.shooterConfig);
+    m_shooterFollower.getConfigurator().apply(Configs.Shooter.shooterConfig);
+
+    m_shooterFollower.setControl(
+        new Follower(m_shooterLeader.getDeviceID(), ShooterConstants.kShooterFollowerAlignment));
+  }
+}
