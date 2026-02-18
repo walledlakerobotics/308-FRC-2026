1d7e2c36b214ec10817c1537078215a9901641db
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-02-18 15:01:26 -0500

    Add setVelocity method to shooter

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.shooter;
 
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
@@ -10,11 +11,17 @@ public class Shooter extends SubsystemBase {
   TalonFX m_shooterLeader = new TalonFX(ShooterConstants.kShooterLeaderCanId);
   TalonFX m_shooterFollower = new TalonFX(ShooterConstants.kShooterFollowerCanId);
 
+  private final VelocityVoltage m_velocityVoltageControl = new VelocityVoltage(0.0);
+
   public Shooter() {
     m_shooterLeader.getConfigurator().apply(Configs.Shooter.shooterConfig);
     m_shooterFollower.getConfigurator().apply(Configs.Shooter.shooterConfig);
 
     m_shooterFollower.setControl(
         new Follower(m_shooterLeader.getDeviceID(), ShooterConstants.kShooterFollowerAlignment));
+  }
+
+  public void setVelocity(double velocity) {
+    m_shooterLeader.setControl(m_velocityVoltageControl.withVelocity(velocity));
   }
 }
