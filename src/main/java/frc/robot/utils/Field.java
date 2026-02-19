a9e7e6084043a3620e4c512b9a82dc0bba1d7154
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-02-19 15:23:12 -0500

    Add flip method

--- src/main/java/frc/robot/utils/Field.java
+++ src/main/java/frc/robot/utils/Field.java
@@ -1,7 +1,25 @@
 package frc.robot.utils;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants.FieldConstants;
+
 public class Field {
   private Field() {
     throw new UnsupportedOperationException("This is a utility class!");
+  }
+
+  /**
+   * Flips a pose across the field. This is useful for converting between the red and blue sides of
+   * the field, as the field is symmetrical.
+   *
+   * @param pose The pose to flip, in field coordinates.
+   * @return The flipped pose, in field coordinates.
+   */
+  public static Pose2d flip(Pose2d pose) {
+    return new Pose2d(
+        FieldConstants.kFieldLengthMeters - pose.getX(),
+        FieldConstants.kFieldWidthMeters - pose.getY(),
+        pose.getRotation().plus(Rotation2d.k180deg));
   }
 }
