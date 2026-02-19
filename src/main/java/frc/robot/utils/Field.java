6fa6d3ef117730b7ca33b70edbbdabf80dbb84d9
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-02-19 15:23:59 -0500

    Add translation overload for flip

--- src/main/java/frc/robot/utils/Field.java
+++ src/main/java/frc/robot/utils/Field.java
@@ -2,6 +2,7 @@ package frc.robot.utils;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.Constants.FieldConstants;
 
 public class Field {
@@ -21,5 +22,18 @@ public class Field {
         FieldConstants.kFieldLengthMeters - pose.getX(),
         FieldConstants.kFieldWidthMeters - pose.getY(),
         pose.getRotation().plus(Rotation2d.k180deg));
+  }
+
+  /**
+   * Flips a translation across the field. This is useful for converting between the red and blue
+   * sides of the field, as the field is symmetrical.
+   *
+   * @param translation The translation to flip, in field coordinates.
+   * @return The flipped translation, in field coordinates.
+   */
+  public static Translation2d flip(Translation2d translation) {
+    return new Translation2d(
+        FieldConstants.kFieldLengthMeters - translation.getX(),
+        FieldConstants.kFieldWidthMeters - translation.getY());
   }
 }
