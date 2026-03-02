c442f5cbe619f77207179ff4ba6ebfd3f9a20d60
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-02 16:16:15 -0500

    Add SysId routine

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -23,6 +23,9 @@ public class Shooter extends SubsystemBase {
 
   private Supplier<Pose2d> robotPoseSupplier;
 
+  private final SysIdRoutine m_sysIdRoutine =
+      new SysIdRoutine(new Config(), new Mechanism(this::setVoltage, null, this));
+
   /** Creates a new Shooter. */
   public Shooter(Supplier<Pose2d> robotPoseSupplier) {
     this.robotPoseSupplier = robotPoseSupplier;
