90b017b1e4ddfe32e1c327ffe324dfbc7f37ccfa
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-03-04 19:20:43 -0500

    Format Feeder.java

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
@@ -9,19 +8,21 @@ import frc.robot.Constants.FeederConstants;
 
 public class Feeder extends SubsystemBase {
 
-  private TalonFX m_motor = new TalonFX(FeederConstants.kFeederCanId); 
-
+  private TalonFX m_motor = new TalonFX(FeederConstants.kFeederCanId);
 
   public Feeder() {
     m_motor.getConfigurator().apply(Configs.Feeder.feederConfig);
-    }
-    public void run(){
-      m_motor.set(FeederConstants.kfeederspeed);
-    }
-    public void stop(){
-      m_motor.set(0.0);
-    }
-    public Command feeder(){
-      return startEnd(this::run, this::stop);
-    }
+  }
+
+  public void run() {
+    m_motor.set(FeederConstants.kfeederspeed);
+  }
+
+  public void stop() {
+    m_motor.set(0.0);
+  }
+
+  public Command feeder() {
+    return startEnd(this::run, this::stop);
+  }
 }
