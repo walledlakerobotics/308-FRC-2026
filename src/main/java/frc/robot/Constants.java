c73c4e8b9c371f572c2d9236b935bf8e7bc5c1e5
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-03-04 19:20:36 -0500

    Format Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -279,7 +279,7 @@ public final class Constants {
     public static final int kRightLightSensorChannel = 0;
   }
 
-  public static final class FeederConstants{
+  public static final class FeederConstants {
     public static final int kFeederCanId = CANIDs.secondaryMotor(0);
     public static final double kFeederMotorReduction = 1.0;
     public static final NeutralModeValue kFeederIntakeNeutralMode = NeutralModeValue.Brake;
@@ -287,6 +287,5 @@ public final class Constants {
     public static final int kFeederMotorSupplyCurrentLimit = 70;
     public static final InvertedValue kFeederInverted = InvertedValue.Clockwise_Positive;
     public static final double kfeederspeed = 0.5;
-    
   }
 }
