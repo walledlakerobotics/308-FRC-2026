2d0c214f7cef414c45bd20b54be47eb5750cefbb
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-08 16:46:58 -0400

    Remove limit switches

--- src/main/java/frc/robot/subsystems/Extender.java
+++ src/main/java/frc/robot/subsystems/Extender.java
@@ -3,11 +3,9 @@ package frc.robot.subsystems;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
 import frc.robot.Constants.ExtenderConstants;
@@ -16,11 +14,6 @@ public class Extender extends SubsystemBase {
 
   private SparkMax m_motor = new SparkMax(ExtenderConstants.kExtenderCanId, MotorType.kBrushless);
   private AbsoluteEncoder m_encoder = m_motor.getAbsoluteEncoder();
-
-  private SparkLimitSwitch m_closedLimitSwitch =
-      m_motor.getForwardLimitSwitch(); // I might make this depend on the angle of the motor,
-  private SparkLimitSwitch m_openedLimitSwitch =
-      m_motor.getReverseLimitSwitch(); // with an absolute encoder.
 
   public Extender() {
     m_motor.configure(
@@ -39,14 +32,6 @@ public class Extender extends SubsystemBase {
     return m_encoder.getVelocity();
   }
 
-  public boolean isExtendorClosed() {
-    return m_closedLimitSwitch.isPressed();
-  }
-
-  public boolean isExtendorOpened() {
-    return m_openedLimitSwitch.isPressed();
-  }
-
   /**
    * Runs the extendor motor at a set speed until limit switch is triggered.
    *
@@ -56,10 +41,6 @@ public class Extender extends SubsystemBase {
     return runOnce(
             () -> {
               m_motor.set(-ExtenderConstants.kExdenterMotorSpeed);
-            })
-        .andThen(
-            () -> {
-              Commands.waitUntil(() -> m_closedLimitSwitch.isPressed());
             })
         .finallyDo(
             () -> {
@@ -77,10 +58,6 @@ public class Extender extends SubsystemBase {
     return runOnce(
             () -> {
               m_motor.set(ExtenderConstants.kExdenterMotorSpeed);
-            })
-        .andThen(
-            () -> {
-              Commands.waitUntil(() -> m_openedLimitSwitch.isPressed());
             })
         .finallyDo(
             () -> {
