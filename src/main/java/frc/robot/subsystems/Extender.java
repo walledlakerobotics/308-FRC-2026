8e04cd12e39426949df54dbed169f68140565bcc
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-08 16:50:16 -0400

    Add profiled PID controller

--- src/main/java/frc/robot/subsystems/Extender.java
+++ src/main/java/frc/robot/subsystems/Extender.java
@@ -5,15 +5,23 @@ import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
 import frc.robot.Constants.ExtenderConstants;
 
 public class Extender extends SubsystemBase {
-
   private SparkMax m_motor = new SparkMax(ExtenderConstants.kExtenderCanId, MotorType.kBrushless);
   private AbsoluteEncoder m_encoder = m_motor.getAbsoluteEncoder();
+
+  private ProfiledPIDController m_pidController =
+      new ProfiledPIDController(
+          ExtenderConstants.kP,
+          ExtenderConstants.kI,
+          ExtenderConstants.kD,
+          new Constraints(ExtenderConstants.kMaxSpeed, ExtenderConstants.kMaxAcceleration));
 
   public Extender() {
     m_motor.configure(
