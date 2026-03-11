d3ee6833389febbc98b7406d97704805391c24f5
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-11 07:37:19 -0400

    Add setVoltage method

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,18 +1,23 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
 import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
-
   private TalonFX m_motor = new TalonFX(IntakeConstants.kIntakeCanId);
+  private VoltageOut m_voltageControl = new VoltageOut(0);
 
   public Intake() {
-
     m_motor.getConfigurator().apply(Configs.Intake.intakeConfig);
+  }
+
+  public void setVoltage(Voltage volts) {
+    m_motor.setControl(m_voltageControl.withOutput(volts));
   }
 
   public void run() {
