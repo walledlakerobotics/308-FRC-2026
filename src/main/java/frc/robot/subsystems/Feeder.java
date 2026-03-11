26f0a7ec651201c9046243b2a09d80efc8e52a70
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-11 07:42:49 -0400

    Add feeder SysId routine

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,16 +1,31 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Mechanism;
 import frc.robot.Configs;
 import frc.robot.Constants.FeederConstants;
 
 public class Feeder extends SubsystemBase {
   private TalonFX m_motor = new TalonFX(FeederConstants.kFeederCanId);
   private VoltageOut m_voltageControl = new VoltageOut(0.0);
+
+  private final SysIdRoutine m_sysIdRoutine =
+      new SysIdRoutine(
+          new Config(
+              null,
+              null,
+              null,
+              state -> {
+                SignalLogger.writeString("sysid-test-state-feeder", state.toString());
+              }),
+          new Mechanism(this::setVoltage, null, this, "feeder"));
 
   public Feeder() {
     m_motor.getConfigurator().apply(Configs.Feeder.feederConfig);
