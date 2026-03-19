68458b1b396b5da74889cf01e13fc21b51173ac6
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-19 19:24:41 -0400

    Switch to using Translation3d for cache

--- src/main/java/frc/robot/subsystems/shooter/math/VirtualTarget.java
+++ src/main/java/frc/robot/subsystems/shooter/math/VirtualTarget.java
@@ -2,10 +2,8 @@ package frc.robot.subsystems.shooter.math;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import frc.robot.Constants.ShooterConstants;
-import frc.robot.utils.Field;
-import frc.robot.utils.Field.Landmark;
 import java.util.HashMap;
 
 /**
@@ -19,7 +17,7 @@ import java.util.HashMap;
 public class VirtualTarget {
   private static final VirtualTarget instance = new VirtualTarget();
 
-  private final HashMap<Landmark, Translation2d> virtualTargetCache = new HashMap<>();
+  private final HashMap<Translation3d, Translation3d> virtualTargetCache = new HashMap<>();
 
   private VirtualTarget() {}
 
@@ -38,39 +36,6 @@ public class VirtualTarget {
   }
 
   /**
-   * Updates the virtual target cache for the scoring targets with the robot pose and robot speeds.
-   * This should be called periodically (e.g. in the robot's periodic method) to ensure the virtual
-   * targets are updated as the robot moves and its speeds change.
-   *
-   * @param robotPose The current position of the robot in field coordinates.
-   * @param robotSpeeds The current field-relative speeds of the robot in the x and y directions in
-   *     meters per second.
-   */
-  public void update(Pose2d robotPose, ChassisSpeeds robotSpeeds) {
-    for (Landmark target : Landmark.values()) {
-      Translation2d virtualTarget =
-          calculateVirtualTarget(
-              target.getTranslation(Field.getAlliance()).toTranslation2d(),
-              robotPose,
-              robotSpeeds,
-              ShooterConstants.kVirtualTargetIterations);
-      virtualTargetCache.put(target, virtualTarget);
-    }
-  }
-
-  /**
-   * Gets the virtual target position for the given target. Assumes update has been called recently
-   * to populate the cache with the latest virtual target positions.
-   *
-   * @param target The original target position in field coordinates.
-   * @return The virtual target position in field coordinates.
-   */
-  public Translation2d getVirtualTarget(Landmark target) {
-    return virtualTargetCache.getOrDefault(
-        target, target.getTranslation(Field.getAlliance()).toTranslation2d());
-  }
-
-  /**
    * Calculates a virtual target position that accounts for the robot's movement during the time of
    * flight of the projectile. This method iteratively calculates the virtual target by estimating
    * how far the robot will travel during the time of flight of the projectile and adjusting the
@@ -85,11 +50,16 @@ public class VirtualTarget {
    * @return The calculated virtual target position in field coordinates that accounts for the
    *     robot's movement during the time of flight of the projectile.
    */
-  public static final Translation2d calculateVirtualTarget(
-      Translation2d target, Pose2d robotPose, ChassisSpeeds robotSpeeds, int iterations) {
+  public final Translation3d calculateVirtualTarget(
+      Translation3d target, Pose2d robotPose, ChassisSpeeds robotSpeeds, int iterations) {
+    if (virtualTargetCache.containsKey(target)) {
+      return virtualTargetCache.get(target);
+    }
+
     Translation2d robotTranslation = robotPose.getTranslation();
 
-    Translation2d virtualTarget = target;
+    Translation2d target2d = target.toTranslation2d();
+    Translation2d virtualTarget = target2d;
 
     for (int i = 0; i < iterations; i++) {
       double distance = robotTranslation.getDistance(virtualTarget);
@@ -99,10 +69,13 @@ public class VirtualTarget {
       double deltaY = robotSpeeds.vyMetersPerSecond * timeOfFlight;
       Translation2d robotDelta = new Translation2d(deltaX, deltaY);
 
-      virtualTarget = target.minus(robotDelta);
+      virtualTarget = target2d.minus(robotDelta);
     }
 
-    return virtualTarget;
+    Translation3d virtualTarget3d =
+        new Translation3d(virtualTarget.getX(), virtualTarget.getY(), target.getZ());
+
+    return virtualTarget3d;
   }
 
   /**
@@ -118,8 +91,8 @@ public class VirtualTarget {
    * @return The calculated virtual target position in field coordinates that accounts for the
    *     robot's movement during the time of flight of the projectile.
    */
-  public static final Translation2d calculateVirtualTarget(
-      Translation2d target, Pose2d robotPose, ChassisSpeeds robotSpeeds) {
+  public final Translation3d calculateVirtualTarget(
+      Translation3d target, Pose2d robotPose, ChassisSpeeds robotSpeeds) {
     return calculateVirtualTarget(target, robotPose, robotSpeeds, 5);
   }
 }
