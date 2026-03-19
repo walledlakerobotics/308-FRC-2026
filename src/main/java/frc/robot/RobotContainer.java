69d89aff7d75b66b4f9a81cf2108154bab1bc3bc
Taran Ghuman <181992731+hydrazine152@users.noreply.github.com>
2026-03-19 19:42:03 -0400

    Remove extra JavaDoc spaces

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,9 +33,9 @@ import frc.robot.utils.MatchTimer;
 import frc.robot.utils.MatchTimer.HubState;
 
 /*
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 @Logged
